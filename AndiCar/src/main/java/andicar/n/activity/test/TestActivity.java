effd9a87eaff51aa6f07db0632fdced878e4fca1
Miki <miklos.keresztes@bitsoftware.ro>
2017-10-24 17:51:21 +0300

    drive integration

--- AndiCar/src/main/java/andicar/n/activity/test/TestActivity.java
+++ AndiCar/src/main/java/andicar/n/activity/test/TestActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,16 +15,28 @@ import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.DriveResource;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.OpenFileActivityBuilder;
 
 import org.andicar2.activity.R;
 
 @SuppressLint("SetTextI18n")
 public class TestActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private static final int RESOLVE_CONNECTION_REQUEST_CODE = 1;
+    private static final int REQUEST_CODE_RESOLVE_CONNECTION = 1;
+    private static final int REQUEST_CODE_OPEN_DRIVE_FILE = 1000;
+    private static final String TAG = "AndiCar";
     private GoogleApiClient mGoogleApiClient;
 
     private TextView text1;
+    private TextView text2;
+    private TextView text3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,8 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
         setContentView(R.layout.test_activity);
 
         text1 = (TextView) findViewById(R.id.text1);
+        text2 = (TextView) findViewById(R.id.text2);
+        text3 = (TextView) findViewById(R.id.text3);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Drive.API)
@@ -48,6 +63,26 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
                 }
             }
         });
+
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+                    IntentSender intentSender = Drive.DriveApi
+                            .newOpenFileActivityBuilder()
+                            .setMimeType(new String[]{DriveFolder.MIME_TYPE, "text/plain", "image/png"})
+                            .build(mGoogleApiClient);
+                    try {
+                        startIntentSenderForResult(
+                                intentSender, REQUEST_CODE_OPEN_DRIVE_FILE, null, 0, 0, 0);
+                    }
+                    catch (IntentSender.SendIntentException e) {
+                        Log.i("AndiCar", "Failed to launch file chooser.");
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -60,7 +95,7 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         if (connectionResult.hasResolution()) {
             try {
-                connectionResult.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST_CODE);
+                connectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLVE_CONNECTION);
             }
             catch (IntentSender.SendIntentException e) {
                 // Unable to resolve, message user appropriately
@@ -72,23 +107,48 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
     }
 
     @Override
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        switch (requestCode) {
-            case RESOLVE_CONNECTION_REQUEST_CODE:
-                if (resultCode == RESULT_OK) {
-                    mGoogleApiClient.connect();
-                }
-                break;
-        }
-    }
-
-    @Override
     public void onConnected(@Nullable Bundle bundle) {
-        text1.setText("Connected");
+        text3.setText("Connected");
     }
 
     @Override
     public void onConnectionSuspended(int i) {
 
     }
-}
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_RESOLVE_CONNECTION:
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+            case REQUEST_CODE_OPEN_DRIVE_FILE:
+                if (resultCode == RESULT_OK) {
+                    DriveId driveId = data.getParcelableExtra(
+                            OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);//this extra contains the drive id of the selected file
+                    Log.i(TAG, "Selected folder's ID: " + driveId.encodeToString());
+                    Log.i(TAG, "Selected folder's Resource ID: " + driveId.getResourceId());
+
+////                    selected file (can also be a folder)
+                    DriveFolder selectedFolder = driveId.asDriveFolder();
+                    PendingResult selectedFileMetadata = selectedFolder.getMetadata(mGoogleApiClient);
+
+//                    fetch the selected item's metadata asynchronously using a pending result
+                    selectedFileMetadata.setResultCallback(new ResultCallback() {
+                        @Override
+                        public void onResult(@NonNull Result result) {
+                            Metadata fileMetadata = ((DriveResource.MetadataResult) result).getMetadata();
+//                            get the details out of the metadata object
+                            Log.i(TAG, "File title: " + fileMetadata.getTitle());
+                            Log.i(TAG, "File size: " + fileMetadata.getFileSize());
+                            Log.i(TAG, "File mime type: " + fileMetadata.getMimeType());
+                            Log.i(TAG, "File mime type: " + fileMetadata.getOriginalFilename());
+                        }
+                    });
+                }
+                break;
+        }
+    }
+    }
