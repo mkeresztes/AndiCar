d2dbb787ed365d9bf0d30f019c4c40ed5348a8bf
Miki <miklos.keresztes@bitsoftware.ro>
2017-10-25 16:19:52 +0300

    upload file to GDrive

--- AndiCar/src/main/java/andicar/n/activity/test/TestActivity.java
+++ AndiCar/src/main/java/andicar/n/activity/test/TestActivity.java
@@ -1,8 +1,10 @@
 package andicar.n.activity.test;
 
+import android.accounts.AccountManager;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -19,21 +21,112 @@ import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveContents;
 import com.google.android.gms.drive.DriveFolder;
 import com.google.android.gms.drive.DriveId;
 import com.google.android.gms.drive.DriveResource;
 import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataChangeSet;
 import com.google.android.gms.drive.OpenFileActivityBuilder;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
 
+import org.andicar2.activity.AndiCar;
 import org.andicar2.activity.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+import andicar.n.persistence.AndiCarFileProvider;
+import andicar.n.utils.ConstantValues;
 
 @SuppressLint("SetTextI18n")
 public class TestActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private static final int REQUEST_CODE_RESOLVE_CONNECTION = 1;
     private static final int REQUEST_CODE_OPEN_DRIVE_FILE = 1000;
     private static final String TAG = "AndiCar";
+    final private ResultCallback<DriveFolder.DriveFileResult> fileCallback = new ResultCallback<DriveFolder.DriveFileResult>() {
+        @Override
+        public void onResult(DriveFolder.DriveFileResult result) {
+            if (!result.getStatus().isSuccess()) {
+                Log.d(TAG, "Error while trying to create the file");
+                return;
+            }
+            Log.d(TAG, "Created a file with content: " + result.getDriveFile().getDriveId());
+        }
+    };
+    GoogleAccountCredential mGoogleCredential;
+    private SharedPreferences mPref = AndiCar.getDefaultSharedPreferences();
     private GoogleApiClient mGoogleApiClient;
+    final private ResultCallback<DriveApi.DriveContentsResult> driveContentsCallback = new ResultCallback<DriveApi.DriveContentsResult>() {
 
+        @Override
+        public void onResult(DriveApi.DriveContentsResult result) {
+
+            if (!result.getStatus().isSuccess()) {
+                Log.d(TAG, "Error while trying to create new file contents");
+                return;
+            }
+
+            final DriveContents driveContents = result.getDriveContents();
+
+            // Perform I/O off the UI thread.
+            new Thread() {
+                @Override
+                public void run() {
+                    OutputStream outputStream = driveContents.getOutputStream();
+                    try {
+                        //getting image from the local storage
+//                /sdcard/andicar/backups/abk_2017-10-17-092529637.db
+                        InputStream inputStream = getContentResolver().openInputStream(
+                                AndiCarFileProvider.getUriForFile(getApplicationContext(), "org.andicar2.provider", new File("/sdcard/andicar/backups/abk_2017-10-17-092529637.db")));
+
+                        if (inputStream != null) {
+                            byte[] data = new byte[1024];
+                            while (inputStream.read(data) != -1) {
+                                //Reading data from local storage and writing to google drive
+                                outputStream.write(data);
+                            }
+                            inputStream.close();
+                        }
+
+                        outputStream.close();
+                    }
+                    catch (IOException e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+
+
+                    MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                            .setTitle("abk_2017-10-17-092529637.db")
+                            .setMimeType("application/octet-stream")
+                            .setStarred(true).build();
+
+
+                    DriveApi.DriveIdResult exFolderResult = Drive.DriveApi
+                            .fetchDriveId(mGoogleApiClient, mPref.getString(getString(R.string.pref_key_google_drive_folder_id), "")) //existing folder id = 0B_cMuo4-XwcAZ3IzSG1jajFlWk0
+                            .await();
+
+                    if (!exFolderResult.getStatus().isSuccess()) {
+                        Log.d(TAG, "Cannot find DriveId. Are you authorized to view this file?");
+                        return;
+                    }
+
+                    final DriveFolder folder = exFolderResult.getDriveId().asDriveFolder();
+
+                    // create a file on root folder
+                    folder.createFile(mGoogleApiClient, changeSet, driveContents)
+                            .setResultCallback(fileCallback);
+
+                }
+            }.start();
+
+        }
+    };
     private TextView text1;
     private TextView text2;
     private TextView text3;
@@ -47,20 +140,27 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
         text2 = (TextView) findViewById(R.id.text2);
         text3 = (TextView) findViewById(R.id.text3);
 
+        mGoogleCredential = GoogleAccountCredential.usingOAuth2(this, Arrays.asList(ConstantValues.GOOGLE_SCOPES)).setBackOff(new ExponentialBackOff());
+        mGoogleCredential.setSelectedAccountName(mPref.getString(getString(R.string.pref_key_google_account), ""));
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Drive.API)
                 .addScope(Drive.SCOPE_FILE)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
+                .setAccountName(mGoogleCredential.getSelectedAccountName())
                 .build();
 
         Button btn1 = (Button) findViewById(R.id.btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-                    mGoogleApiClient.clearDefaultAccountAndReconnect();
-                }
+//                if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+//                    mGoogleApiClient.clearDefaultAccountAndReconnect();
+//                }
+                text1.setText("");
+                startActivityForResult(
+                        mGoogleCredential.newChooseAccountIntent(), ConstantValues.REQUEST_ACCOUNT_PICKER);
             }
         });
 
@@ -81,6 +181,14 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
                         Log.i("AndiCar", "Failed to launch file chooser.");
                     }
                 }
+            }
+        });
+
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Drive.DriveApi.newDriveContents(mGoogleApiClient).setResultCallback(driveContentsCallback);
             }
         });
     }
@@ -108,7 +216,8 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        text3.setText("Connected");
+        text1.setText("Connected to: " + mGoogleCredential.getSelectedAccountName());
+        text2.setText(mPref.getString(getString(R.string.pref_key_google_drive_folder_name), ""));
     }
 
     @Override
@@ -119,6 +228,19 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         switch (requestCode) {
+            case ConstantValues.REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK) {
+                    mGoogleCredential.setSelectedAccountName(data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME));
+                    mGoogleApiClient = new GoogleApiClient.Builder(this)
+                            .addApi(Drive.API)
+                            .addScope(Drive.SCOPE_FILE)
+                            .addConnectionCallbacks(this)
+                            .addOnConnectionFailedListener(this)
+                            .setAccountName(mGoogleCredential.getSelectedAccountName())
+                            .build();
+                    mGoogleApiClient.connect();
+                }
+                break;
             case REQUEST_CODE_RESOLVE_CONNECTION:
                 if (resultCode == RESULT_OK) {
                     mGoogleApiClient.connect();
@@ -130,6 +252,9 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
                             OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);//this extra contains the drive id of the selected file
                     Log.i(TAG, "Selected folder's ID: " + driveId.encodeToString());
                     Log.i(TAG, "Selected folder's Resource ID: " + driveId.getResourceId());
+                    SharedPreferences.Editor e = mPref.edit();
+                    e.putString(getString(R.string.pref_key_google_drive_folder_id), driveId.getResourceId());
+                    e.apply();
 
 ////                    selected file (can also be a folder)
                     final DriveFolder selectedFolder = driveId.asDriveFolder();
@@ -140,6 +265,10 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
                         @Override
                         public void onResult(@NonNull Result result) {
                             Metadata fileMetadata = ((DriveResource.MetadataResult) result).getMetadata();
+                            text2.setText(fileMetadata.getTitle());
+                            SharedPreferences.Editor e = mPref.edit();
+                            e.putString(getString(R.string.pref_key_google_drive_folder_name), fileMetadata.getTitle());
+                            e.apply();
 //                            get the details out of the metadata object
                             Log.i(TAG, "File title: " + fileMetadata.getTitle());
                             Log.i(TAG, "File size: " + fileMetadata.getFileSize());
@@ -150,4 +279,4 @@ public class TestActivity extends AppCompatActivity implements GoogleApiClient.C
                 break;
         }
     }
-    }
+}
