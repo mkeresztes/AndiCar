c7ab12f877e8924a4efcbb7c67905f21979b9ec9
Miki <miklos.keresztes@bitsoftware.ro>
2018-02-06 21:04:40 +0200

    FB messaging

--- AndiCar/src/main/java/andicar/n/activity/dialogs/MessageDialog.java
+++ AndiCar/src/main/java/andicar/n/activity/dialogs/MessageDialog.java
@@ -2,9 +2,14 @@ package andicar.n.activity.dialogs;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.andicar2.activity.R;
@@ -22,33 +27,54 @@ public class MessageDialog extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setFinishOnTouchOutside(false);
+
         setContentView(R.layout.dialog_message);
-        TextView title = findViewById(R.id.tvTitle);
-        TextView body = findViewById(R.id.tvBody);
+        TextView tvTitle = findViewById(R.id.tvTitle);
+        TextView tvBody = findViewById(R.id.tvBody);
+        Button btnClose = findViewById(R.id.btnClose);
 
         Bundle extras = getIntent().getExtras();
         if(extras == null){
-            title.setText(getString(R.string.error_127));
+            tvTitle.setText(getString(R.string.error_127));
             return;
         }
 
         DBAdapter dbAdapter = new DBAdapter(this);
         long msgRowId = dbAdapter.getMessageRowId(extras.getString(MSG_ID_KEY));
         if(msgRowId == -1){
-            title.setText(getString(R.string.error_127));
+            tvTitle.setText(getString(R.string.error_127));
             dbAdapter.close();
             return;
         }
 
         Cursor c = dbAdapter.fetchRecord(DBAdapter.TABLE_NAME_MESSAGES, DBAdapter.COL_LIST_MESSAGES_TABLE, msgRowId);
         if(c == null){
-            title.setText(getString(R.string.error_127));
+            tvTitle.setText(getString(R.string.error_127));
             dbAdapter.close();
             return;
         }
 
-        title.setText(c.getString(DBAdapter.COL_POS_GEN_NAME));
-        body.setText(c.getString(DBAdapter.COL_POS_GEN_USER_COMMENT));
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //close the dialog
+                MessageDialog.this.finish();
+            }
+        });
+
+
+        tvTitle.setBackgroundResource(R.drawable.ui_message_header_shape);
+        tvTitle.setText(c.getString(DBAdapter.COL_POS_GEN_NAME));
+        tvBody.setMovementMethod(LinkMovementMethod.getInstance());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            tvBody.setText(Html.fromHtml(c.getString(DBAdapter.COL_POS_GEN_USER_COMMENT), Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            tvBody.setText(Html.fromHtml(c.getString(DBAdapter.COL_POS_GEN_USER_COMMENT)));
+        }
+//        tvBody.setText(c.getString(DBAdapter.COL_POS_GEN_USER_COMMENT));
 
         c.close();
 
